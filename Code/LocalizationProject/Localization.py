1ef2586cdca04541767c1034b9d5bd3b3b16f8ca
zstauscher <57776051+zstauscher@users.noreply.github.com>
2022-04-28 12:49:07 -0400

    Update localization to return x and y coordinates

--- Code/LocalizationProject/Localization.py
+++ Code/LocalizationProject/Localization.py
@@ -19,10 +19,10 @@ def localization():
         #print (Mic_tuning_0.direction)
         #print (Mic_tuning_1.direction)
         x, y = calculateLocation.calculate_distance(6, Mic_tuning_0.direction, Mic_tuning_1.direction)
-        print(x,y)
-        #return (x, y)
+        #print(x,y)
+        return (x, y)
 
-
+"""
         while True:
             try:
 
@@ -34,5 +34,5 @@ def localization():
 
             except KeyboardInterrupt:
                 break
-
-localization()
+"""
+#localization()
